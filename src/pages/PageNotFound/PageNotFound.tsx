26dcc33a92d685989abbf7e20e0f8b801e62071c
Rafael Ramos <junior07.rrn@hotmail.com>
2024-02-23 18:07:23 -0500

    Add NavBar and hero section in homepage

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
+import "./PageNotFound.scss";
+// import videoBg from "../bg-video.mp4";
+
+
 export default function PageNotFound() {
-  return <h1>Page not found: 404</h1>;
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main className="page">
+        <h1 className="page__header">Pagina no encontrada: 404</h1>
+        <p>
+          Regresar a <Link to="/">Home</Link>
+        </p>
+      </main>
+    </>
+  );
 }
